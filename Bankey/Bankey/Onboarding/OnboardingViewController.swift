762cd9f12d25f109ba50defd49cb7be0f40082f9
Samuel <yusheng593@gmail.com>
2024-08-15 13:45:31 +0800

    add 通用的 OnboardingViewController() 包含 image and title

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -11,10 +11,21 @@ class OnboardingViewController: UIViewController {
 
     let stackView = UIStackView()
     let imageView = UIImageView()
-    let imageName = "delorean"
+    let heroImageName: String
     let label = UILabel()
-    let labelText = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in the 80s."
+    let titleText: String
 
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
@@ -31,14 +42,14 @@ extension OnboardingViewController {
 
         // Image
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: imageName)
+        imageView.image = UIImage(named: heroImageName)
 
         // Label
         label.textAlignment = .center
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = labelText
+        label.text = titleText
     }
 
     func layout() {
