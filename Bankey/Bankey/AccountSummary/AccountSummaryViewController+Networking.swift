52bb8df8de829c13e0f4346869c77ca42840bd8f
Samuel <yusheng593@gmail.com>
2024-08-29 09:23:24 +0800

    網路請求的錯誤處理

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController+Networking.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController+Networking.swift
@@ -26,21 +26,21 @@ struct Profile:Codable {
 
 extension AccountSummaryViewController {
     func fetchProfile(forUserId userId:String, completion:@escaping (Result<Profile,NetworkError>) -> Void) {
-        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                completion(.failure(.serverError))
-                return
-            }
-
-            do {
-                let profile = try JSONDecoder().decode(Profile.self, from: data)
-                completion(.success(profile))
-            } catch {
+//        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            guard let data = data, error == nil else {
+//                completion(.failure(.serverError))
+//                return
+//            }
+//
+//            do {
+//                let profile = try JSONDecoder().decode(Profile.self, from: data)
+//                completion(.success(profile))
+//            } catch {
                 completion(.failure(.decodingError))
-            }
-        }.resume()
+//            }
+//        }.resume()
     }
 }
 
